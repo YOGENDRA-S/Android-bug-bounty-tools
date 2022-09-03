c7b6ba57e4d1f8dcd450f22b1cc7a11106478870
Yogendra Singh <106531785+YOGENDRA-S@users.noreply.github.com>
2022-09-03 00:15:46 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
+
+
+apt-get -y update
+
+apt-get -y upgrade
+
+apt-get instally libcurl4-openssl-dev
+
+apt-get install -y libssl-dev
+
+apt-get install -y jq
+
+apt-get install -y ruby-full
+
+apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+
+apt-get install -y build-essential libssl-dev libffi-dev python-dev
+
+apt-get install -y python-setuptools
+
+apt-get install -y libldns-dev
+
+apt-get install -y python3-pip
+
+apt-get install -y python-pip
+
+apt-get install -y python-dnspython
+
+apt-get install -y git
+
+apt-get install -y rename
+
+apt-get install -y xargs
+
+apt-get install golang
 
